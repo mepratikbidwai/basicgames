a39b3e1b377015ee2cd720486f52eb25955a5207
Pratik Bidwai <thebrahmaputra0510@gmail.com>
2017-10-06 00:14:30 +0530

    First-commit

---
+++ DemoClass.cpp
@@ -0,0 +1,14 @@
+/*
+ * DemoClass.cpp
+ *
+ *  Created on: 29-Jul-2017
+ *      Author: pappu
+ */
+
+#include "DemoClass.h"
+
+DemoClass::DemoClass() {
+	// TODO Auto-generated constructor stub
+
+}
+// TODO Auto-generated destructor stub
---
+++ DemoClass.h
@@ -0,0 +1,17 @@
+/*
+ * DemoClass.h
+ *
+ *  Created on: 29-Jul-2017
+ *      Author: Pratik
+ */
+
+#ifndef DEMOCLASS_H_
+#define DEMOCLASS_H_
+
+class DemoClass {
+public:
+	DemoClass();
+
+};
+
+#endif /* DEMOCLASS_H_ */
